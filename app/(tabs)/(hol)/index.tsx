a349ed2df80c27f9f5b453f42619e5f4b4af8c81
_deltabrain <marconator13@gmail.com>
2024-07-23 16:46:20 +0200

    .

--- app/(tabs)/(hol)/index.tsx
+++ app/(tabs)/(hol)/index.tsx
@@ -1,10 +1,111 @@
 import { ThemedView } from '@/components/ThemedView';
-import { StyleSheet, Image } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
+import { CardView } from '@/components/Card';
+import { useEffect } from 'react';
+
+enum Suit {
+	Spades = 'spades',
+	Hearts = 'hearts',
+	Diamonds = 'diamonds',
+	Clubs = 'clubs',
+}
+
+enum Value {
+	Two = 'two_of_',
+	Three = 'three_of_',
+	Four = 'four_of_',
+	Five = 'five_of_',
+	Six = 'six_of_',
+	Seven = 'seven_of_',
+	Eight = 'eight_of_',
+	Nine = 'nine_of_',
+	Ten = 'ten_of_',
+	Jack = 'jack_of_',
+	Queen = 'queen_of_',
+	King = 'king_of_',
+	Ace = 'ace_of_',
+}
+
+interface Card {
+	suit: Suit;
+	value: Value;
+}
+
+const newDeck: Card[] = [
+	{ suit: Suit.Spades, value: Value.Ace },
+	{ suit: Suit.Spades, value: Value.Two },
+	{ suit: Suit.Spades, value: Value.Three },
+	{ suit: Suit.Spades, value: Value.Four },
+	{ suit: Suit.Spades, value: Value.Five },
+	{ suit: Suit.Spades, value: Value.Six },
+	{ suit: Suit.Spades, value: Value.Seven },
+	{ suit: Suit.Spades, value: Value.Eight },
+	{ suit: Suit.Spades, value: Value.Nine },
+	{ suit: Suit.Spades, value: Value.Ten },
+	{ suit: Suit.Spades, value: Value.Jack },
+	{ suit: Suit.Spades, value: Value.Queen },
+	{ suit: Suit.Spades, value: Value.King },
+
+	{ suit: Suit.Hearts, value: Value.Ace },
+	{ suit: Suit.Hearts, value: Value.Two },
+	{ suit: Suit.Hearts, value: Value.Three },
+	{ suit: Suit.Hearts, value: Value.Four },
+	{ suit: Suit.Hearts, value: Value.Five },
+	{ suit: Suit.Hearts, value: Value.Six },
+	{ suit: Suit.Hearts, value: Value.Seven },
+	{ suit: Suit.Hearts, value: Value.Eight },
+	{ suit: Suit.Hearts, value: Value.Nine },
+	{ suit: Suit.Hearts, value: Value.Ten },
+	{ suit: Suit.Hearts, value: Value.Jack },
+	{ suit: Suit.Hearts, value: Value.Queen },
+	{ suit: Suit.Hearts, value: Value.King },
+
+	{ suit: Suit.Diamonds, value: Value.Ace },
+	{ suit: Suit.Diamonds, value: Value.Two },
+	{ suit: Suit.Diamonds, value: Value.Three },
+	{ suit: Suit.Diamonds, value: Value.Four },
+	{ suit: Suit.Diamonds, value: Value.Five },
+	{ suit: Suit.Diamonds, value: Value.Six },
+	{ suit: Suit.Diamonds, value: Value.Seven },
+	{ suit: Suit.Diamonds, value: Value.Eight },
+	{ suit: Suit.Diamonds, value: Value.Nine },
+	{ suit: Suit.Diamonds, value: Value.Ten },
+	{ suit: Suit.Diamonds, value: Value.Jack },
+	{ suit: Suit.Diamonds, value: Value.Queen },
+	{ suit: Suit.Diamonds, value: Value.King },
+
+	{ suit: Suit.Clubs, value: Value.Ace },
+	{ suit: Suit.Clubs, value: Value.Two },
+	{ suit: Suit.Clubs, value: Value.Three },
+	{ suit: Suit.Clubs, value: Value.Four },
+	{ suit: Suit.Clubs, value: Value.Five },
+	{ suit: Suit.Clubs, value: Value.Six },
+	{ suit: Suit.Clubs, value: Value.Seven },
+	{ suit: Suit.Clubs, value: Value.Eight },
+	{ suit: Suit.Clubs, value: Value.Nine },
+	{ suit: Suit.Clubs, value: Value.Ten },
+	{ suit: Suit.Clubs, value: Value.Jack },
+	{ suit: Suit.Clubs, value: Value.Queen },
+	{ suit: Suit.Clubs, value: Value.King },
+];
+
+var currentDeck: Card[] = newDeck;
+
+function shuffleDeck(deck: Card[]) {
+	for (let i = deck.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[deck[i], deck[j]] = [deck[j], deck[i]];
+	}
+}
 
 export default function Teufeln() {
+	shuffleDeck(currentDeck);
 	return (
 		<ThemedView style={styles.default}>
-			<Image source={require('@/assets/images/card_png/')} />;
+			<Button title="Higher" onPress={() => console.log('Higher')} />
+			<Button title="Lower" onPress={() => console.log('Lower')} />
+			<CardView card={currentDeck[0].value + currentDeck[0].suit} />
+			<Button title="Shuffle" onPress={() => shuffleDeck(currentDeck)} />
 		</ThemedView>
 	);
 }
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,61 @@
 // TODO: lookup table for cards
+import { Image } from 'react-native';
 
-export default function Card({ card }: { card: string }) {
-	return <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 16 16"></svg>;
+const cardImages: Record<string, any> = {
+	ace_of_spades: require('@/assets/images/cards/ace_of_spades.png'),
+	ace_of_hearts: require('@/assets/images/cards/ace_of_hearts.png'),
+	ace_of_diamonds: require('@/assets/images/cards/ace_of_diamonds.png'),
+	ace_of_clubs: require('@/assets/images/cards/ace_of_clubs.png'),
+	two_of_spades: require('@/assets/images/cards/two_of_spades.png'),
+	two_of_hearts: require('@/assets/images/cards/two_of_hearts.png'),
+	two_of_diamonds: require('@/assets/images/cards/two_of_diamonds.png'),
+	two_of_clubs: require('@/assets/images/cards/two_of_clubs.png'),
+	three_of_spades: require('@/assets/images/cards/three_of_spades.png'),
+	three_of_hearts: require('@/assets/images/cards/three_of_hearts.png'),
+	three_of_diamonds: require('@/assets/images/cards/three_of_diamonds.png'),
+	three_of_clubs: require('@/assets/images/cards/three_of_clubs.png'),
+	four_of_spades: require('@/assets/images/cards/four_of_spades.png'),
+	four_of_hearts: require('@/assets/images/cards/four_of_hearts.png'),
+	four_of_diamonds: require('@/assets/images/cards/four_of_diamonds.png'),
+	four_of_clubs: require('@/assets/images/cards/four_of_clubs.png'),
+	five_of_spades: require('@/assets/images/cards/five_of_spades.png'),
+	five_of_hearts: require('@/assets/images/cards/five_of_hearts.png'),
+	five_of_diamonds: require('@/assets/images/cards/five_of_diamonds.png'),
+	five_of_clubs: require('@/assets/images/cards/five_of_clubs.png'),
+	six_of_spades: require('@/assets/images/cards/six_of_spades.png'),
+	six_of_hearts: require('@/assets/images/cards/six_of_hearts.png'),
+	six_of_diamonds: require('@/assets/images/cards/six_of_diamonds.png'),
+	six_of_clubs: require('@/assets/images/cards/six_of_clubs.png'),
+	seven_of_spades: require('@/assets/images/cards/seven_of_spades.png'),
+	seven_of_hearts: require('@/assets/images/cards/seven_of_hearts.png'),
+	seven_of_diamonds: require('@/assets/images/cards/seven_of_diamonds.png'),
+	seven_of_clubs: require('@/assets/images/cards/seven_of_clubs.png'),
+	eight_of_spades: require('@/assets/images/cards/eight_of_spades.png'),
+	eight_of_hearts: require('@/assets/images/cards/eight_of_hearts.png'),
+	eight_of_diamonds: require('@/assets/images/cards/eight_of_diamonds.png'),
+	eight_of_clubs: require('@/assets/images/cards/eight_of_clubs.png'),
+	nine_of_spades: require('@/assets/images/cards/nine_of_spades.png'),
+	nine_of_hearts: require('@/assets/images/cards/nine_of_hearts.png'),
+	nine_of_diamonds: require('@/assets/images/cards/nine_of_diamonds.png'),
+	nine_of_clubs: require('@/assets/images/cards/nine_of_clubs.png'),
+	ten_of_spades: require('@/assets/images/cards/ten_of_spades.png'),
+	ten_of_hearts: require('@/assets/images/cards/ten_of_hearts.png'),
+	ten_of_diamonds: require('@/assets/images/cards/ten_of_diamonds.png'),
+	ten_of_clubs: require('@/assets/images/cards/ten_of_clubs.png'),
+	jack_of_spades: require('@/assets/images/cards/jack_of_spades.png'),
+	jack_of_hearts: require('@/assets/images/cards/jack_of_hearts.png'),
+	jack_of_diamonds: require('@/assets/images/cards/jack_of_diamonds.png'),
+	jack_of_clubs: require('@/assets/images/cards/jack_of_clubs.png'),
+	queen_of_spades: require('@/assets/images/cards/queen_of_spades.png'),
+	queen_of_hearts: require('@/assets/images/cards/queen_of_hearts.png'),
+	queen_of_diamonds: require('@/assets/images/cards/queen_of_diamonds.png'),
+	queen_of_clubs: require('@/assets/images/cards/queen_of_clubs.png'),
+	king_of_spades: require('@/assets/images/cards/king_of_spades.png'),
+	king_of_hearts: require('@/assets/images/cards/king_of_hearts.png'),
+	king_of_diamonds: require('@/assets/images/cards/king_of_diamonds.png'),
+	king_of_clubs: require('@/assets/images/cards/king_of_clubs.png'),
+};
+
+export function CardView({ card }: { card: string }) {
+	return <Image source={cardImages[card]} style={{ width: 320, height: 464 }} />;
 }
