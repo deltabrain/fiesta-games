a78d88cdd1720cf7c14eb76a9ebff4e78b4a8fad
Deltabrain <marconator13@gmail.com>
2024-08-12 10:22:23 +0200

    refactoring

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -2,13 +2,10 @@ import { View, type ViewProps } from 'react-native';
 
 import { useThemeColor } from '@/hooks/useThemeColor';
 
-export type ThemedViewProps = ViewProps & {
-	lightColor?: string;
-	darkColor?: string;
-};
+export type ThemedViewProps = ViewProps & {};
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
+export function ThemedView({ style, ...rest }: ThemedViewProps) {
 	const backgroundColor = useThemeColor('background');
 
-	return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+	return <View style={[{ backgroundColor }, style]} {...rest} />;
 }
