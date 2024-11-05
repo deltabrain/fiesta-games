2dd261d4e74a123c1fe89b547905381e5df0782a
Deltabrain <marconator13@gmail.com>
2024-11-05 12:18:33 +0100

    chore: cleanup

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,13 +1,7 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
 export const Colors = {
-	// TODO: change light Colors to actually support light mode
 	light: {
 		text: '#07090a',
 		fadedText: '#182024',
