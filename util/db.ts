26fae017fe6f7c6ae17b487a48a8b5795b0bf010
Deltabrain <marconator13@gmail.com>
2024-11-13 23:58:00 +0100

    dev: db not working

--- util/db.ts
+++ util/db.ts
@@ -1,23 +1,14 @@
 import firestore from '@react-native-firebase/firestore';
 
 const db = firestore();
-const userCollection = db.collection('Users');
 const boardsCollection = db.collection('Boards');
 
-export function getFields(id: string | undefined) {
+export async function getFields(id: string | undefined) {
 	if (!id) {
 		return;
 	}
 	const board: any[] = [];
-	for (var i = 0; i < 9; i++) {
-		boardsCollection
-			.doc(id)
-			.collection('Fields')
-			.doc(i.toString())
-			.get()
-			.then((f) => (board[i] = f.data()))
-			.catch((err) => console.error(err));
-		// TODO: read and write data to firestore
-	}
+	await boardsCollection.doc(id).collection('Fields').doc('0').get();
+	// TODO: read and write data to firestore
 	return board;
 }
