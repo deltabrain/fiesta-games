187bcd7eb9bf8eb798c1dfccaae53acfea364957
Deltabrain <marconator13@gmail.com>
2024-12-06 02:18:49 +0100

    chore: linting

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,6 +1,6 @@
-import { useThemeColor } from '../hooks/useThemeColor';
-import { ThemedText } from './themed/ThemedText';
-import { ThemedView } from './themed/ThemedView';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import { ThemedText } from '@/themed/ThemedText';
+import { ThemedView } from '@/themed/ThemedView';
 
 export function Error() {
 	const textColor = useThemeColor('warning');
