0d0b48e16a75c6d94bc806f9bf3a435de0a67621
deltabrain <marcobusch1998@gmail.com>
2025-07-01 16:01:36 +0200

    fix: obfuscate api key with eas env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,12 +2,10 @@ import type { Database } from '@lib/supabase.types';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-// TODO: obfuscate these values
-const supabaseUrl = 'https://qfpdgpcfbwaflfvzdtgz.supabase.co';
-const supbaseAnonKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmcGRncGNmYndhZmxmdnpkdGd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIyNjQ1MDgsImV4cCI6MjA0Nzg0MDUwOH0.inma8XP7zmNbqaZkc8jNT1x-ssUxTWDuvIh1cfJzpUg';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supbaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient<Database>(supabaseUrl, supbaseAnonKey, {
+export const supabase = createClient<Database>(supabaseUrl!, supbaseAnonKey!, {
 	auth: {
 		storage: AsyncStorage,
 		autoRefreshToken: true,
